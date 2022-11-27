97f5645775535c53992d5d4af9bf01e18470ce27
rsyd29 <brasyid15@gmail.com>
2022-11-27 22:21:14 +0700

    Change Padding on CardContentsWidget

--- lib/app/modules/home/widgets/card_contents_widget.dart
+++ lib/app/modules/home/widgets/card_contents_widget.dart
@@ -12,7 +12,6 @@ class CardContentsWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8.0),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(6),
@@ -34,42 +33,50 @@ class CardContentsWidget extends StatelessWidget {
           data.thumbnail != null
               ? CachedNetworkImage(imageUrl: data.thumbnail!)
               : const SizedBox.shrink(),
-          Text(
-            data.title,
-            style: const TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 18.0,
-            ),
-          ),
-          Text(
-            '[${data.media}]',
-            style: const TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            'Dimasukkan oleh ${data.contributor} - ${timeago.format(
-              data.createdAt,
-              locale: 'id',
-            )}',
-            style: const TextStyle(
-              color: Colors.grey,
-            ),
-          ),
-          data.body != null
-              ? Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 8.0,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  data.title,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
                   ),
-                  child: Text(
-                    data.body!,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 5,
+                ),
+                Text(
+                  '[${data.media}]',
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
-                )
-              : const SizedBox.shrink(),
+                ),
+                Text(
+                  'Dimasukkan oleh ${data.contributor} - ${timeago.format(
+                    data.createdAt,
+                    locale: 'id',
+                  )}',
+                  style: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                data.body != null
+                    ? Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8.0,
+                        ),
+                        child: Text(
+                          data.body!,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 5,
+                        ),
+                      )
+                    : const SizedBox.shrink(),
+              ],
+            ),
+          ),
         ],
       ),
     );
